cb3d05d2905ca3d2b2d7d3408a899819a904f5b2
Kex027 <87434868+Kex027@users.noreply.github.com>
2022-06-29 20:45:06 +0200

    Added react-wordle project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Board from "./components/navbar/Board";
+import Keyboard from "./components/navbar/Keyboard";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Keyboard />
+      <Board />
+    </div>
+  );
+};
+
+export default App;
+//rsc
